6e1109cfdaf218757dc627803dce3b4db65502f8
Andrew Mittleider <andrew.mittleider@walnut.ai>
2019-06-17 13:25:31 +0200

    Always runs tests 2 times

---
+++ level_2_project/my_level_2_project/tests/test_level_2_class.py
@@ -0,0 +1,8 @@
+from my_level_2_project.code import Level2Class
+from unittest import TestCase
+
+
+class TestLevel2Class(TestCase):
+    def test_level_2_class(self):
+        Level2Class()
+        pass
